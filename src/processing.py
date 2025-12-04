fd6860a2250d3dcadbfd6f667bc8901ba1dce3e9
why_code <119089018+VWiman@users.noreply.github.com>
2025-12-04 12:30:25 +0100

    Basic function implemented

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
 from src import DATA_DIR
 from os import walk
 
-
+# Create a .txt file for each image file (with the same name) in directory and write the filename to it
 def createtxt():
     filenames = next(walk(DATA_DIR), (None, None, []))[2]
     for i in filenames:
